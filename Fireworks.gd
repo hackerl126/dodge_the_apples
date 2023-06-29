5dfe0bdd8ee742b1d81f9efafec7a3f39f258669
hackerl126 <hackerl@126.com>
2023-06-29 20:15:08 +0800

    Create Fireworks.gd

---
+++ Fireworks.gd
@@ -0,0 +1,9 @@
+extends Node
+
+func _ready():
+	$Control1/Firework1.emitting=true
+	$Control2/Firework2.emitting=true
+	$Control3/Firework3.emitting=true
+	$Control4/Firework4.emitting=true
+	$Control5/Firework5.emitting=true                                       
+
